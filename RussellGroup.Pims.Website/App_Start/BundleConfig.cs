4cf2d91c7cb557030dc0007eb3a63409df6dbcce
Brett <digitalrizzle@gmail.com>
2014-07-18 21:06:52 +1200

    Organised CSS and JS bundles

--- RussellGroup.Pims.Website/App_Start/BundleConfig.cs
+++ RussellGroup.Pims.Website/App_Start/BundleConfig.cs
@@ -8,30 +8,49 @@ namespace RussellGroup.Pims.Website
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootstrap-datepicker.js",
+                "~/Scripts/view-datepickers.js",
+                "~/Scripts/respond.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //    "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/DataTables-1.9.4/media/js/jquery.dataTables.js",
-                      "~/Scripts/bootstrap-datepicker.js",
-                      "~/Scripts/bloodhound.js",
-                      "~/Scripts/handlebars.js",
-                      "~/Scripts/typeahead.bundle.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery.datatable.js").Include(
+                "~/Scripts/DataTables-1.9.4/media/js/jquery.dataTables.js",
+                "~/Scripts/dataTables.bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-theme.css",
-                      "~/Content/DataTables-1.9.4/media/css/jquery.dataTables.css",
-                      "~/Content/bootstrap-datepicker.css",
-                      "~/Content/typeahead.css",
-                      "~/Content/Site.css"));
+             bundles.Add(new ScriptBundle("~/bundles/jquery.typeahead.js").Include(
+                "~/Scripts/bloodhound.js",
+                "~/Scripts/handlebars.js",
+                "~/Scripts/typeahead.bundle.js",
+                "~/Scripts/view-typeahead.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/bootstrap.css", new CssRewriteUrlTransformWrapper())
+                .Include("~/Content/bootstrap-theme.css", new CssRewriteUrlTransformWrapper())
+                .Include("~/Content/bootstrap-datepicker.css", new CssRewriteUrlTransformWrapper()));
+
+            bundles.Add(new StyleBundle("~/Content/jquery.datatable.css")
+                .Include("~/Content/DataTables-1.9.4/media/css/jquery.dataTables.css", new CssRewriteUrlTransformWrapper()));
+
+            bundles.Add(new StyleBundle("~/Content/jquery.typeahead.css")
+                .Include("~/Content/typeahead.css", new CssRewriteUrlTransformWrapper()));
+
+            bundles.Add(new StyleBundle("~/Content/overrides.css")
+                .Include("~/Content/Site.css", new CssRewriteUrlTransformWrapper()));
+        }
+
+        public class CssRewriteUrlTransformWrapper : IItemTransform
+        {
+            public string Process(string includedVirtualPath, string input)
+            {
+                return new CssRewriteUrlTransform().Process("~" + VirtualPathUtility.ToAbsolute(includedVirtualPath), input);
+            }
         }
     }
 }
--- RussellGroup.Pims.Website/Scripts/view-user-form.js
+++ RussellGroup.Pims.Website/Scripts/view-user-form.js
@@ -1,21 +1,21 @@
-﻿    $(document).ready(function () {
-        $('#roles-table').dataTable({
-            "bStateSave": false,
-            "bFilter": false,
-            "bInfo": false,
-            "bPaginate": false,
-            "aoColumns": [
-                { "sWidth": "0%" },
-                { "sWidth": "5%" },
-                { "sWidth": "20%" },
-                { "sWidth": "75%" }
-            ],
-            "aoColumnDefs": [
-                { "bVisible": false, "aTargets": [0] },
-                { "bSortable": false, "aTargets": [1] }
-            ]
-        });
+﻿$(document).ready(function () {
+    $('#roles-table').dataTable({
+        "bStateSave": false,
+        "bFilter": false,
+        "bInfo": false,
+        "bPaginate": false,
+        "aoColumns": [
+            { "sWidth": "0%" },
+            { "sWidth": "5%" },
+            { "sWidth": "20%" },
+            { "sWidth": "75%" }
+        ],
+        "aoColumnDefs": [
+            { "bVisible": false, "aTargets": [0] },
+            { "bSortable": false, "aTargets": [1] }
+        ]
     });
+});
 
 // adjusts the datatable container because pagination is hidden
 $('[id$="-table"]').on('draw.dt', function () {
