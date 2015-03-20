d8ec40b91c8bae470ffbd7c1f353cfb717ccfb59
Brett <digitalrizzle@gmail.com>
2015-03-20 17:34:32 +1300

    Bug fixes
    Fixed a bug where the datepicker would populate a date when the input
    box was cleared and any key was pressed. Also fixed a bug on the check
    in page where if the filter was applied (on either plant or inventory)
    then upon submission any selected items hidden by the filter were not
    included.

--- RussellGroup.Pims.Website/Scripts/view-datepickers.js
+++ RussellGroup.Pims.Website/Scripts/view-datepickers.js
@@ -1,44 +1,40 @@
 ﻿// WhenStarted/WhenEnded
 
-var whenStarted = $("#WhenStarted").datepicker({
-    format: "d/mm/yyyy"
-}).on("changeDate", function (e) {
-    whenStarted.hide();
-}).data("datepicker");
-
-var whenEnded = $("#WhenEnded").datepicker({
-    format: "d/mm/yyyy"
-}).on("changeDate", function (e) {
-    whenEnded.hide();
-}).data("datepicker");
+$("#WhenStarted").datepicker({
+    format: "d/mm/yyyy",
+    autoclose: true
+});
 
 $("#WhenStarted-icon").click(function () {
-    whenStarted.show();
+    $("#WhenStarted").datepicker("show");
+});
+
+$("#WhenEnded").datepicker({
+    format: "d/mm/yyyy",
+    autoclose: true
 });
 
 $("#WhenEnded-icon").click(function () {
-    whenEnded.show();
+    $("#WhenEnded").datepicker("show");
 });
 
 
 // WhenPurchased/WhenDisused
 
-var whenPurchased = $("#WhenPurchased").datepicker({
-    format: "d/mm/yyyy"
-}).on("changeDate", function (e) {
-    whenPurchased.hide();
-}).data("datepicker");
-
-var whenDisused = $("#WhenDisused").datepicker({
-    format: "d/mm/yyyy"
-}).on("changeDate", function (e) {
-    whenDisused.hide();
-}).data("datepicker");
+$("#WhenPurchased").datepicker({
+    format: "d/mm/yyyy",
+    autoclose: true
+});
 
 $("#WhenPurchased-icon").click(function () {
-    whenPurchased.show();
+    $("#WhenPurchased").datepicker("show");
+});
+
+$("#WhenDisused").datepicker({
+    format: "d/mm/yyyy",
+    autoclose: true
 });
 
 $("#WhenDisused-icon").click(function () {
-    whenDisused.show();
+    $("#WhenDisused").datepicker("show");
 });
