d8dafd5fcb9ce9bacd888fb4d2041db62b95d94c
Brett <digitalrizzle@gmail.com>
2015-10-11 19:39:59 +1300

    Added SQL seeding templates (again!)
    There was a gitignore which stopped these from being committed.

---
+++ RussellGroup.Pims.DataAccess/Migrations/Sql/SetUserNameContext.sql
@@ -0,0 +1,11 @@
+﻿IF EXISTS (SELECT * FROM sys.objects WHERE object_id = object_id(N'[dbo].[SetContextUserName]') AND type IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[SetContextUserName]
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = object_id(N'[dbo].[SetContextUserName]') AND type IN (N'P', N'PC'))
+EXEC dbo.sp_executesql @statement =
+N'CREATE PROCEDURE [dbo].[SetContextUserName](@userName NVARCHAR(50)) AS
+BEGIN
+	DECLARE @m BINARY(128)
+	SET @m = CAST(@userName AS BINARY(128))
+	SET CONTEXT_INFO @m
+END'
